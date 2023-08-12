8ecf375fbc07ee988595c979e14816114c8c50e3
osawa-koki <koki@osawa.dev>
2023-08-12 13:56:54 +0900

    S3バケットの存在確認。

--- app/handler.rb
+++ app/handler.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
+require 'dotenv'
+
+require_relative './existence_check'
+
+$s3_bucket_name = nil
+
+def init
+  Dotenv.load
+  $s3_bucket_name = ENV['S3_BUCKET_NAME']
+end
+
 def handler
-  puts 'Hello from handler.rb'
+  init
+
+  existence_check
 end
--- main.rb
+++ main.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-require './app/handler'
+require_relative './app/handler'
 
 handler
